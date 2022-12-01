6c741cf2c6302f4cf27a572840ef03ecb266d20d
shaffer2323 <94629362+shaffer2323@users.noreply.github.com>
2022-12-01 11:00:20 -0600

    Update sketch.js

--- Unit4/mp4/sketch.js
+++ Unit4/mp4/sketch.js
@@ -72,19 +72,7 @@ function draw() {
         timer = 0;
         state = 3;
       }
-      textSize(20);
-  fill('black');
-  text("orientation data:", 25, 25);
-  textSize(15);
-  text("alpha: " + alpha, 25, 50);
-  text("beta: " + beta, 25, 70);
-  text("gamma: " + gamma, 25, 90);
-  textSize(20);
-  text("acceleration data:", 25, 125);
-  textSize(15);
-  text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
-  text("y = " + y.toFixed(2), 25, 170);
-  text("z = " + z.toFixed(4), 25, 190);
+   
       break;
 
     case 2: // win screen
