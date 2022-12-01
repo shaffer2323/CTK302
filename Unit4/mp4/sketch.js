480f029ad0da995ed5181a55574414b14682c348
shaffer2323 <94629362+shaffer2323@users.noreply.github.com>
2022-12-01 11:20:59 -0600

    Update sketch.js

--- Unit4/mp4/sketch.js
+++ Unit4/mp4/sketch.js
@@ -199,13 +199,13 @@ class player{
     }
   }
   move(){
-    //push(); // before you use translate, rotate, or scale commands, push and then pop after
+    push(); // before you use translate, rotate, or scale commands, push and then pop after
 
-    //translate(xPosition, yPosition); // move everything over by x, y
+    translate(xPosition, yPosition); // move everything over by x, y
   
-    //rotate(radians(alpha)); // rotate the bunny depending on the alpha intake
+    rotate(radians(alpha)); // rotate the bunny depending on the alpha intake
   
-    //pop();
+    pop();
   }
 }
 
